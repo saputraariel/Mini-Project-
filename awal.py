5130747483260e154872008e893efb8086fbea73
saputraariel <saputraariel39@gmail.com>
2024-09-26 13:25:57 +0800

    Add files via upload

---
+++ awal.py
@@ -0,0 +1,33 @@
+print ("-----------------------------")
+print ("NAMA = Mohamad Ariel saputra d loi")
+print ("NIM = 2409116087")
+print ("KELAS = Sistem informasi C")
+print ("-----------------------------")
+
+# x = 5
+# y = 10
+
+# hasil = x + y
+# perbandingan = x > y
+# hehe = x == y
+# heheh = x != y
+# kalian = x * y
+
+# print (hasil)
+# print (perbandingan)
+# print (hehe)
+# print (heheh)
+# print (kalian)
+# x *= 100
+# print(x)
+
+
+r = 3.14
+a = float(input("Masukkan jari jari: "))
+
+print((r) * (a) * (a))
+
+
+
+
+
